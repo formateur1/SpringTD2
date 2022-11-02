ad0f8bb6d980fc54e02539cb4a7a34f1371eca3c
formateur1 <enseignant.formateur@protonmail.com>
2022-11-02 17:04:21 +0100

    Début du projet

---
+++ src/main/java/com/inti/SpringTd2Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTd2Application.class, args);
+	}
+
+}
