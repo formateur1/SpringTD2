53c284f58291cdc545bd5309ed5112ee9058a5be
formateur1 <enseignant.formateur@protonmail.com>
2022-11-03 10:51:36 +0100

    Ajout des opérations CRUD pour Produit

--- src/main/java/com/inti/controller/ProduitController.java
+++ src/main/java/com/inti/controller/ProduitController.java
@@ -1,10 +1,20 @@
 package com.inti.controller;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.inti.model.Produit;
 import com.inti.repository.IProduitRepository;
+
 
 @RestController
 @RequestMapping("produit")
@@ -13,6 +23,48 @@ public class ProduitController {
 	@Autowired
 	IProduitRepository ipr;
 	
+	@GetMapping("produits")
+	public List<Produit> getProduits()
+	{
+		return ipr.findAll();
+	}
 	
+	@PostMapping("saveProduit")
+	public boolean saveProduit(@RequestBody Produit produit)
+	{
+		if(produit != null)
+		{
+			ipr.save(produit);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	@GetMapping("produit/{id}")
+	public Produit getProduit(@PathVariable int id)
+	{
+		Produit p;
+		try {
+			p = ipr.findById(id).get();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			p = null;
+		}
+		return p;
+	}
+	
+	@DeleteMapping("deleteProduit/{id}")
+	public boolean deleteProduit(@PathVariable int id)
+	{
+		if(id > 0)
+		{
+			ipr.deleteById(id);
+			return true;
+		}
+		
+		return false;
+	}
 
 }
